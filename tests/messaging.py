db87fff5bc594989e4c380399cf6358c364852f1
Fec <fectung@gmail.com>
2018-05-07 19:13:11 +0800

    Use get_keycode to simplify the function

--- tests/messaging.py
+++ tests/messaging.py
@@ -10,64 +10,64 @@ from time import sleep
 from appium import webdriver
 from conf.appium_config import cfg, logging
 from conf import appium_config
-from common.utils import wait_el_xpath, wait_el_xpath_click
+from common.utils import wait_el_xpath, wait_el_xpath_click, get_keycode
 
 class Messaging(unittest.TestCase):
   @classmethod
   def setUpClass(self):
     self.driver = appium_config.my_webdriver('Messaging')
 
-  def test_SMS_Mo(self):
+  def initM(self):
+    """
+    Init messaging's recipients and make text editor prepared
+    """
     wait_el_xpath_click(self.driver, cfg.get('messaging', 'btn_create_path'))
     recipients = wait_el_xpath(self.driver, cfg.get('messaging', 'recipients_path'))
-    text_editor = wait_el_xpath(self.driver, cfg.get('messaging', 'text_editor_path'))
-    btn_send_sms = wait_el_xpath(self.driver, cfg.get('messaging', 'btn_send_sms_path'))
     recipients.clear()
-    self.driver.press_keycode(8)
-    self.driver.press_keycode(11)
-    self.driver.press_keycode(14)
-    self.driver.press_keycode(15)
-    self.driver.press_keycode(9)
-    self.driver.press_keycode(10)
-    self.driver.press_keycode(7)
-    self.driver.press_keycode(12)
-    self.driver.press_keycode(10)
-    self.driver.press_keycode(11)
-    self.driver.press_keycode(15)
-    self.driver.press_keycode(66)
+
+    # phone number: 147 8230 5348
+    num = [
+      get_keycode(1), get_keycode(4), get_keycode(7),
+      get_keycode(8), get_keycode(2), get_keycode(3), get_keycode(0), 
+      get_keycode(5), get_keycode(3), get_keycode(4), get_keycode(8)
+    ]
+    for n in num:
+      self.driver.press_keycode(n)
+
+    text_editor = wait_el_xpath(self.driver, cfg.get('messaging', 'text_editor_path'))
+    return text_editor
+
+  def test_SMS_Mo(self):
+    logging.info('test_SMS_Mo: START')
+
+    text_editor = self.initM()
+    
+    btn_send_sms = wait_el_xpath(self.driver, cfg.get('messaging', 'btn_send_sms_path'))
     text_editor.click()
     text_editor.send_keys(cfg.get('default', 'default_msg'))
     btn_send_sms.click()
+    logging.info('test_SMS_Mo: Send a SMS.')
     sleep(5)
     wait_el_xpath_click(self.driver, cfg.get('messaging', 'navigate_up_path'))
+    logging.info('test_SMS_Mo: END')
 
   def test_MMS_MO(self):
-    wait_el_xpath_click(self.driver, cfg.get('messaging', 'btn_create_path'))
-    recipients = wait_el_xpath(self.driver, cfg.get('messaging', 'recipients_path'))
-    recipients.clear()
-    self.driver.press_keycode(8)
-    self.driver.press_keycode(11)
-    self.driver.press_keycode(14)
-    self.driver.press_keycode(15)
-    self.driver.press_keycode(9)
-    self.driver.press_keycode(10)
-    self.driver.press_keycode(7)
-    self.driver.press_keycode(12)
-    self.driver.press_keycode(10)
-    self.driver.press_keycode(11)
-    self.driver.press_keycode(15)
-    self.driver.press_keycode(66)
+    logging.info('test_MMS_MO: START')
+
+    text_editor = self.initM()
+
     wait_el_xpath_click(self.driver, cfg.get('messaging', 'add_attach_path'))
     wait_el_xpath_click(self.driver, cfg.get('messaging', 'attach_subject_path'))
     et_subject = wait_el_xpath(self.driver, cfg.get('messaging', 'et_subject_path'))
-    text_editor = wait_el_xpath(self.driver, cfg.get('messaging', 'text_editor_path'))
     et_subject.send_keys(cfg.get('default', 'default_subject'))
     text_editor.click()
     text_editor.send_keys(cfg.get('default', 'default_msg'))
     btn_send_mms = wait_el_xpath(self.driver, cfg.get('messaging', 'btn_send_mms_path'))
     btn_send_mms.click()
+    logging.info('test_SMS_Mo: Send a MMS.')
     sleep(5)
     wait_el_xpath_click(self.driver, cfg.get('messaging', 'navigate_up_path'))
+    logging.info('test_MMS_MO: END')
 
   @classmethod
   def tearDownClass(self):
