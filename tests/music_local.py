a8e7b424048600b589c947b49f05537c8e1cc466
Fec <fectung@gmail.com>
2018-05-07 19:13:28 +0800

    Add logging info

--- tests/music_local.py
+++ tests/music_local.py
@@ -16,14 +16,18 @@ class GoogleMusic(unittest.TestCase):
     self.driver = appium_config.my_webdriver('GoogleMusic')
   
   def test_music_palyback(self):
+    logging.info('test_music_palyback: START')
     wait_el_xpath(self.driver, cfg.get('music', 'btn_skip_path'))
     wait_el_xpath_click(self.driver, cfg.get('music', 's'))
     wait_el_xpath_click(self.driver, cfg.get('music', 'shuffle_all_path'))
+    logging.info('test_music_palyback: Shuffle all of the music.')
     btn_pause_play = wait_el_xpath(self.driver, cfg.get('music', 'btn_pause_play_path'))
     wait_el_xpath_click(self.driver, cfg.get('music', 'btn_menu_path'))
     
     while btn_pause_play.tag_name == cfg.get('music', 'status_playing') :
-      logging.info(btn_pause_play.tag_name)
+      logging.debug(btn_pause_play.tag_name)
+      
+    logging.info('test_music_palyback: END')
 
   @classmethod
   def tearDownClass(self):
