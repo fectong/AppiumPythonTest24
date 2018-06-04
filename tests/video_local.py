a6af1b638d08a425f061f0aca64612bf31b2d825
Fec <fectung@gmail.com>
2018-06-04 11:27:14 +0800

    Handle permisson and RESUME

--- tests/video_local.py
+++ tests/video_local.py
@@ -6,7 +6,7 @@ import time
 
 sys.path.append("..")
 from conf import appium_config
-from aptools.apconstants import Commands, C_Video
+from aptools.apconstants import Commands, C_Video, C_Location
 from aptools.aputils import action, value, logging, wait_el_xpath, wait_el_xpath_click, wait_els_xpath
 
 
@@ -18,32 +18,32 @@ class Video(unittest.TestCase):
   def test_video_playback(self):
     prefix = C_Video.PREFIX
     play_minutes = C_Video.PLAY_MINUTES
-    
+
     logging.info('{0}: START'.format(prefix))
-    btn_always_x = C_Video.BTN_ALWAYS_X
-    btn_always_y = C_Video.BTN_ALWAYS_Y
-    btn_always_x1 = C_Video.BTN_ALWAYS_X1
-    btn_always_y1 = C_Video.BTN_ALWAYS_Y1
-    
+
     time.sleep(3)
     videos = wait_els_xpath(self.driver, C_Video.PATH_VIDEOS)
     if videos is None:
       logging.info('{0}: Check if there are videos'.format(prefix))
       self.fail("{0}: No Videos".format(prefix))
-    
-    select_play = wait_el_xpath(self.driver, C_Video.PATH_SELECT_PLAY)
-    if select_play is not None:
-      os.popen('adb shell input tap {0} {1}'.format(btn_always_x1, btn_always_y1))
-    else:
-      os.popen('adb shell input tap {0} {1}'.format(btn_always_x, btn_always_y))
-    
+
     logging.debug('{0}: Play each video for {1} minutes'.format(prefix, play_minutes/3))
     for video in videos:
       logging.info('{0}: {1} is playing'.format(prefix, value(video, Commands.TEXT)))
-      timeout = time.time() + 20*play_minutes
-      action(video, Commands.CLICK)
+      time.sleep(1)
 
-      if wait_el_xpath_click(self.driver, C_Video.PATH_START_OVER, 5):
+      action(video, Commands.CLICK)
+      timeout = time.time() + 10*play_minutes
+
+      select_play = wait_el_xpath(self.driver, C_Video.PATH_SELECT_PLAY, 3)
+      if select_play is not None:
+        action(select_play, Commands.CLICK)
+      wait_el_xpath_click(self.driver, C_Video.PATH_BTN_ALWAYS, 2)
+
+      if wait_el_xpath_click(self.driver, C_Location.BTN_ALLOW, 3):
+        logging.info('{0}: Permission Allowed.'.format(prefix))
+
+      if wait_el_xpath_click(self.driver, C_Video.PATH_START_OVER, 3):
         logging.info('{0}: Start Over'.format(prefix))
 
       while time.time() < timeout:
@@ -51,7 +51,11 @@ class Video(unittest.TestCase):
           self.driver.get_window_size()
           logging.info('{0}: Playing'.format(prefix))
           time.sleep(5)
-      self.driver.back()
+
+      if wait_el_xpath(self.driver, C_Video.PATH_VIDEOS) is None:
+        self.driver.back()
+      else:
+        continue
 
     logging.info('{0}: END'.format(prefix))
 
