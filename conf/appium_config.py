0670480148a742b1df5bc35ae08763e578325d98
FecTung <fectung@gmail.com>
2018-06-08 14:16:00 +0800

    Simplify code

--- conf/appium_config.py
+++ conf/appium_config.py
@@ -16,29 +16,35 @@ def my_webdriver(
   app,
   host = '127.0.0.1',
   port = 4723,
-  device_index = 0,
+  device_name = devices_id[0],
   auto_grant_permissions = True,
   no_reset = False,
   full_reset = False,
   system_port = 8200,
   newCommandTimeout = 60,
   app_path = ''):
-  return webdriver.Remote('http://{0}:{1}/wd/hub'.format(host, port), get_desired_caps(app, device_index, auto_grant_permissions, no_reset, full_reset, system_port, newCommandTimeout, app_path))
+  
+  try:
+    return webdriver.Remote('http://{0}:{1}/wd/hub'.format(host, port), get_desired_caps(app, device_name, auto_grant_permissions, no_reset, full_reset, system_port, newCommandTimeout, app_path))
+  except Exception as e:
+    logging.info('please confirm if the appium server is running.')
+    logging.debug('Exception: {0}, please confirm if the appium server is running.'.format(e))
+    return None
 
-def get_desired_caps(app, device_index, auto_grant_permissions, no_reset, full_reset, system_port, newCommandTimeout, app_path):
+def get_desired_caps(app, device_name, auto_grant_permissions, no_reset, full_reset, system_port, newCommandTimeout, app_path):
   desired_caps = {
     'appPackage': app[1],
     'appActivity': app[2],
     'platformName': 'Android',
-    'platformVersion': os_version(devices_id[device_index]),
-    'deviceName': devices_id[device_index],
+    'platformVersion': os_version(device_name),
+    'deviceName': device_name,
     'autoGrantPermissions': auto_grant_permissions,
     'noReset': no_reset,
     'fullReset': full_reset,
     'systemPort': system_port,
     'newCommandTimeout': newCommandTimeout,
     'app': app_path,
-    'udid': devices_id[device_index],
+    'udid': device_name,
     # 'unicodeKeyboard': True,
     # 'resetKeyboard': True,
     'noSign': True,
